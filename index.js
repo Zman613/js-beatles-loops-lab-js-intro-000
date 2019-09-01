480144b8be09bfe726193a785187bbcfe1c17e86
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-09-01 20:58:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(musiciansArray, instrumentsArray){
+  var mixUp = []
+  for (var i = 0; i < musiciansArray.length; i++){
+    mixUp.push(`${musiciansArray[i]} plays ${instrumentsArray[i]}`)
+  }
+  return mixUp
+}
+
+function johnLennonFacts()
